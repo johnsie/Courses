242c11063b47553aef25c1c7af3dcd5a1e9f71ed
John McCourt <john.mccourt000@gmail.com>
2023-12-04 20:19:11 +0000

    Create 001 - Console.WriteLine.cs
    Explain the difference between Console.Write() and Console.WriteLine() methods.

---
+++ freecodecamp/foundational-c-sharp-with-microsoft/001 - Console.WriteLine.cs
@@ -0,0 +1,11 @@
+//First lesson about C# in this course involved printing lines to the console
+
+//Print the literal string "Hello World!" to the console
+Console.Write("Hello World!");
+
+
+Console.WriteLine("This is on one line.");
+Console.Write("This is on another line");
+
+//The difference between the Write and WriteLine methods is that WriteLine appends a newline to the end.
+
