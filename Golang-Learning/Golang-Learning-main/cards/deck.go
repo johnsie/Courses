30a683f0772cf571dcbd95625cd9ba0f8246cae8
John McCourt <john.mccourt000@gmail.com>
2023-12-13 10:44:08 +0000

    first commit for golang practice

---
+++ Golang-Learning/Golang-Learning-main/cards/deck.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	//use underscore if you're never going to use the variable for anything else
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+
+		}
+
+	}
+
+	return cards
+
+} //end function newDeck
+
+//convention to use the first letter of the type
+func (d deck) print() {
+
+	for i, card := range d {
+
+		fmt.Println(i, card)
+	}
+
+}
