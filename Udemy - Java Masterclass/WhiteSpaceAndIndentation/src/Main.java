931eb9d2fdf882dc7460ec20798dec9e29ef43d8
John McCourt <john.mccourt000@gmail.com>
2023-12-21 21:06:10 +0000

    add comments to whitespace and indentation code

---
+++ Udemy - Java Masterclass/WhiteSpaceAndIndentation/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+//See Google Java Style guid for recommendations of good use of white space
+    public static void main(String[] args) {
+
+
+        //Whitespace usually used in Java for readability purposes, so even the following will compile
+
+        int             myVariable =
+
+                50
+
+                ;
+            myVariable++;
+            myVariable--;
+
+        System.out.println("This is a test");
+
+      System.out.println("This is" +
+                " another" +
+                " still more.");
+
+
+    }
+
+
+}
