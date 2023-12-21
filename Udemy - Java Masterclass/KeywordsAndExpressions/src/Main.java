a1cef654986299cbc55a2463f06053a3064d16ec
John McCourt <john.mccourt000@gmail.com>
2023-12-21 20:53:12 +0000

    create java expressions and keywords project

---
+++ Udemy - Java Masterclass/KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args){
+
+    double kilometers = (100 *  1.609344);
+    int highScore = 50;
+
+    if (highScore > 25 ){
+
+        highScore = 1000 + highScore; //add bonus points
+    }//end if
+
+
+    int health = 100;
+
+         //express         //express
+    if ((health <25 )&& (highScore> 1000))
+    {
+
+        highScore = highScore - 1000;
+
+    }//end if
+
+    }//end void main
+
+}//End class
