ffee7ae194105a6437ea9b8d8500fbf07426479d
John McCourt <john.mccourt000@gmail.com>
2023-12-21 21:20:25 +0000

    Add if keyword and codeblocks project

---
+++ Udemy - Java Masterclass/IfKeywordAndCodeBlocks/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args){
+        boolean gameOver=true;
+        int score = 800;
+        int levelCompleted = 5;
+
+        int bonus = 100;
+
+        if (score<5000 && score > 1000){
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        }
+        else if (score < 1000)
+        {
+           System.out.println("Your score was less than 1000");
+        }
+         else
+        {
+            //same as default in a switch statement.
+            System.out.println("Got here");
+        }
+
+
+
+
+    }//end main void
+}//End class main
