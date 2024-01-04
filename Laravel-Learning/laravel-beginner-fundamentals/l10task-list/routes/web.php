a93517adc7bb46b81c99b8e9574a087cb336f9e3
John McCourt <john.mccourt000@gmail.com>
2024-01-04 22:09:35 +0000

    add laravel form route

--- Laravel-Learning/laravel-beginner-fundamentals/l10task-list/routes/web.php
+++ Laravel-Learning/laravel-beginner-fundamentals/l10task-list/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Models\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,66 +15,51 @@ use Illuminate\Http\Response;
 |
 */
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
 
 
-Route::get('tasks/', function () use($tasks) {
+
+Route::get('tasks/', function ()  {
   return view('index',[
-  'tasks' => $tasks
+  'tasks' => \App\Models\Task::latest()->get()
   ]);
  //  return "Shamone!";
 })->name('tasks.index');
+
+
+
+//See laravel documentation for db query builder
+//You can also use php artisan tink to run these queries
+
+Route::get('completed-tasks/', function ()  {
+    return view('index',[
+    'tasks' => \App\Models\Task::latest()->where('completed',true)->get()
+    ]);
+   //  return "Shamone!";
+  })->name('tasks.index');
+  
+  
+  Route::view('/tasks/create', 'create');
+
+  Route::post('/tasks', function (Request $request) {
+        //dd($request->all());
+        
+        //Get the validated data from the form
+        $data = $request->validate([
+        'title'=> 'required|max:255',
+        'description' => 'required',
+         'long_description'=> 'required'
+        ]);
+
+$task = new Task;
+$task->title = $data['title'];
+$task->description = $data['description'];
+$task->long_description= $data['long_description'];
+
+$task->save();
+
+return redirect('')->route('tasks.show',['id' => $task->id]);
+  })->name('tasks.store');
+
 
 
 Route::get('/',function(){
@@ -82,19 +69,12 @@ Route::get('/',function(){
 });
 
 
-Route::get('tasks/{id}', function($id) use($tasks) 
+Route::get('tasks/{id}', function($id) 
 {
-   $task = collect($tasks)->firstWhere('id',$id);
+  //\App\Models\Task::find($id);
 
 
-
-if (!$task){
-
-    abort(Response::HTTP_NOT_FOUND);
-
-}
-
-return view('show',['task' => $task]);
+return view('show',['task' =>  \App\Models\Task::findOrFail($id)]);
 }
 )->name('tasks.show');
 
